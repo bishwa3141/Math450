a82c60dde50e3d4220de7d5059d479d5db54f873
Roberto De Leo <roberto.deleo@howard.edu>
2016-10-16 22:26:28 -0400

    changed almost everything, the previous code was way too naif

--- NonlinearEquations/bisection.py
+++ NonlinearEquations/bisection.py
@@ -1,28 +1,33 @@
 #!/usr/local/sage/local/bin/python
 import sys
 from math import *
+from numpy import sign
+import numpy as np
 
 def f(x):
-    return x**2 - 4*sin(x)
+    return tan(x)
 	
 def bisection(a,b,tol):
     i = 1
-    c = (a+b)/2.0
-    while (b-a)/2.0 > tol:
-        print("iter %d --> a=%e, b=%e, f(a) = %e, f(b) = %e\n" % (i,a,b,f(a),f(b)))
+    hl = (b-a)/2.0
+    c = a+hl
+    seq=[c];
+    while hl > tol:
+        print("iter %d --> c=%1.10e, f(c) = %1.10e\n" % (i,c,f(c)) )
+        if sign(f(a)) != sign(f(c)):
+            b=c
+        else:
+            a=c
+        hl=(b-a)/2.0
+        c = a+hl
         i += 1
-	if f(c) == 0:
-	    return c
-	elif f(a)*f(c) < 0:
-	    b = c
-	else :
-	    a = c
-	c = (a+b)/2.0
-    print("iter %d --> a=%e, b=%e, f(a) = %e, f(b) = %e\n" % (i,a,b,f(a),f(b)))
-    return c
+        seq.append(c)
+    print("iter %d --> c=%1.10e, f(c) = %1.10e\n" % (i,c,f(c)) )
+    return seq
 	
 if len(sys.argv) != 4:
     sys.exit('Usage: bisection.py <a> <b> <tol>')
 
 x = bisection(float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]))
-print("x = %e\n" % x);
+print("x = %1.10e\n" % x[-1]);
+
