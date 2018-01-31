5e426d40c9d807e340a7f4fc78cf7cf001951815
Roberto De Leo <roberto.deleo@howard.edu>
2018-01-31 00:36:43 -0500

    added code for quadratic approximation of the discrete derivative

--- FloatingPoint/discrderiv.m
+++ FloatingPoint/discrderiv.m
@@ -1,8 +1,8 @@
-% A concrete demosntration of the effect of catastrophic cancellation
-% in the evaluation of the discrete derivatives -- cannot take the paramete h too small!
+% A concrete demonstration of the effect of catastrophic cancellation
+% in the evaluation of the discrete derivatives -- cannot take the parameter h too small!
 % You can run this script from the CLI with <matlab -nosplash -nodesktop -r "run discrderiv.m">
 
-function myF()
+function discrderiv()
   
   N=14;
   
@@ -10,10 +10,14 @@ function myF()
     h(i)=10^(-i);
     dder(i)=dsin(1,h(i));
     err(i)=abs(dder(i)-cos(1));
+    dder2(i)=dsin2(1,h(i));
+    err2(i)=abs(dder2(i)-cos(1));
   end
   
   figure(1);
   loglog(h,err,'ro--');
+  hold on;
+  loglog(h,err2,'b*--');
   title('Error in the evaluation of the Discrete Derivative');
   xlabel('h');
   ylabel('Error');
@@ -22,4 +26,8 @@ end
 
 function der = dsin(x,h)
   der = (sin(x+h)-sin(x))/h;
+end
+
+function der = dsin2(x,h)
+  der = (sin(x+h)-sin(x-h))/(2*h);
 end
